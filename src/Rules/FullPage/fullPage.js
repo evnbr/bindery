51128ddde6826ad0ea487356f9d1a79fb3fa25fc
Evan Brooks <brooks.ev@gmail.com>
2017-02-25 12:16:25 -0800

    Rename elementpath

--- src/Rules/FullPage/fullPage.js
+++ src/Rules/FullPage/fullPage.js
@@ -1,16 +1,18 @@
 import css from "style!css!./fullPage.css";
 
+let prevPage, prevElementPath;
+
 export default {
   beforeAdd: (elmt, state) => {
-    state.prevPage = state.currentPage;
-    state.prevElementPath = state.elPath;
+    prevPage = state.currentPage;
+    prevElementPath = state.path;
     state.currentPage = state.getNewPage();
     if (elmt.classList.contains("bleed")) {
       state.currentPage.element.classList.add("bleed");
     }
   },
   afterAdd: (elmt, state) => {
-    state.currentPage = state.prevPage;
-    state.elPath = state.prevElementPath;
+    state.currentPage = prevPage;
+    state.path = prevElementPath;
   },
 }
