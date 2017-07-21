e14e2738f5033d43fbd2d8a67d32ebe93faa227c
Evan Brooks <brooks.ev@gmail.com>
2017-07-21 02:59:34 -0700

    Factor out hypersrcipt components, follow linter

--- src/Controls/controls.js
+++ src/Controls/controls.js
@@ -1,30 +1,42 @@
-import css from "./controls.css";
-import convertUnits from "../utils/convertUnits"
-import h from "hyperscript";
+import h from 'hyperscript';
+import convertUnits from '../utils/convertUnits';
 
-const btn = function() {
-  return h("button.bindery-btn", ...arguments);
-}
-const btnMini = function() {
-  return h("button.bindery-btn.bindery-btn-mini", ...arguments);
-}
-const btnMain = function() {
-  return h("button.bindery-btn.bindery-btn-main", ...arguments);
-}
-const toggle = function() {
-  return h(".bindery-toggle", ...arguments, toggleSwitch);
-}
-const toggleSwitch = function() {
-  return h(".bindery-switch", h(".bindery-switch-handle"));
-}
-const label = function() {
-  return h(".bindery-label", ...arguments);
-}
+require('./controls.css');
+
+
+const select = function (...arg) {
+  return h('select', ...arg);
+};
+const option = function (...arg) {
+  return h('option', ...arg);
+};
+const inputNumber = function (val) {
+  return h('input', { type: 'number', value: val });
+};
+const btn = function (...arg) {
+  return h('button.bindery-btn', ...arg);
+};
+const btnMini = function (...arg) {
+  return h('button.bindery-btn.bindery-btn-mini', ...arg);
+};
+const btnMain = function (...arg) {
+  return h('button.bindery-btn.bindery-btn-main', ...arg);
+};
+const toggleSwitch = () => h('.bindery-switch', h('.bindery-switch-handle'));
+const switchRow = function (...arg) {
+  return h('.bindery-toggle', ...arg, toggleSwitch);
+};
+const row = function (...arg) {
+  return h('.bindery-toggle', ...arg);
+};
+const label = function (...arg) {
+  return h('.bindery-label', ...arg);
+};
 
 
 class Controls {
   constructor(opts) {
-    this.holder = h("div.bindery-controls");
+    this.holder = h('div.bindery-controls');
     document.body.appendChild(this.holder);
 
     this.binder = opts.binder;
@@ -32,179 +44,182 @@ class Controls {
     const done = () => {
       this.binder.cancel();
     };
-    const guides = () => {
-      guidesToggle.classList.toggle("selected")
-      this.binder.viewer.toggleGuides();
-    };
-    const facing = () => {
-      facingToggle.classList.toggle("selected")
-      layoutControl.classList.toggle("not-facing")
-      this.binder.viewer.toggleDouble();
-    };
     const print = () => {
       this.binder.viewer.setPrint();
       window.print();
-    }
+    };
 
-    let printBtn      = btnMain({onclick: print}, "Print");
-    let guidesToggle  = toggle({onclick: guides}, "Show Bounds");
+    const printBtn = btnMain({ onclick: print }, 'Print');
 
-    let facingToggle  = toggle({onclick: facing}, "Facing Pages");
-    facingToggle.classList.add("selected");
+    let layoutControl;
+    let guidesToggle;
+    let facingToggle;
 
-    let doneBtn = "";
+    const toggleGuides = () => {
+      guidesToggle.classList.toggle('selected');
+      this.binder.viewer.toggleGuides();
+    };
+    guidesToggle = switchRow({ onclick: toggleGuides }, 'Show Bounds');
+
+    const toggleFacing = () => {
+      facingToggle.classList.toggle('selected');
+      layoutControl.classList.toggle('not-facing');
+      this.binder.viewer.toggleDouble();
+    };
+    facingToggle = switchRow({ onclick: toggleFacing }, 'Facing Pages');
+    facingToggle.classList.add('selected');
+
+    let doneBtn = '';
     if (!this.binder.runImmeditately) {
-      doneBtn = btn({onclick: done}, "Done");
+      doneBtn = btn({ onclick: done }, 'Done');
     }
 
     const input = {
-      top:    h("input", { type: "number", value: this.binder.pageMargin.top }),
-      inner:  h("input", { type: "number", value: this.binder.pageMargin.inner }),
-      outer:  h("input", { type: "number", value: this.binder.pageMargin.outer }),
-      bottom: h("input", { type: "number", value: this.binder.pageMargin.bottom }),
-      width:  h("input", { type: "number", value: this.binder.pageSize.width }),
-      height: h("input", { type: "number", value: this.binder.pageSize.height })
-    }
+      top: inputNumber(this.binder.pageMargin.top),
+      inner: inputNumber(this.binder.pageMargin.inner),
+      outer: inputNumber(this.binder.pageMargin.outer),
+      bottom: inputNumber(this.binder.pageMargin.bottom),
+      width: inputNumber(this.binder.pageSize.width),
+      height: inputNumber(this.binder.pageSize.height),
+    };
 
-    const sizeControl   = h(".bindery-val.bindery-size",
-      h("div", "W", input.width),
-      h("div", "H", input.height),
+    const sizeControl = h('.bindery-val.bindery-size',
+      h('div', 'W', input.width),
+      h('div', 'H', input.height),
     );
 
-    const unitSelect = h("select",
-      { onchange: function() {
-          changeUnit(this.value);
-      }},
-      h("option", { value: "px" }, "Pixels"),
-      h("option", { disabled: true }, "96px = 1 in"),
-      h("option", { disabled: true }, ""),
-      h("option", { value: "pt" }, "Points"),
-      h("option", { disabled: true }, "72pt = 1 in"),
-      h("option", { disabled: true }, ""),
-      h("option", { value: "pc" }, "Pica"),
-      h("option", { disabled: true }, "6pc = 72pt = 1in"),
-      h("option", { disabled: true }, ""),
-      h("option", { value: "in" }, "Inches"),
-      h("option", { disabled: true }, "1in = 96px"),
-      h("option", { disabled: true }, ""),
-      h("option", { value: "cm" }, "cm"),
-      h("option", { disabled: true }, "2.54cm = 1in"),
-      h("option", { disabled: true }, ""),
-      h("option", { value: "mm" }, "mm"),
-      h("option", { disabled: true }, "25.4mm = 1in"),
-    );
     const changeUnit = (newUnit) => {
       const oldUnit = this.binder.pageUnit;
-      for (let key in input) {
-        let el = input[key];
-        let newVal = convertUnits(parseFloat(el.value), oldUnit, newUnit);
-        let rounded = Math.round(newVal * 100) / 100
+      Object.values(input).forEach((inputEl) => {
+        const el = inputEl;
+        const newVal = convertUnits(parseFloat(el.value), oldUnit, newUnit);
+        const rounded = Math.round(newVal * 100) / 100;
         el.value = rounded;
-      }
+      });
       this.binder.pageUnit = newUnit;
-    }
+    };
+
+    const unitSelect = select(
+      { onchange() {
+        changeUnit(this.value);
+      } },
+      option({ value: 'px' }, 'Pixels'),
+      option({ disabled: true }, '96px = 1 in'),
+      option({ disabled: true }, ''),
+      option({ value: 'pt' }, 'Points'),
+      option({ disabled: true }, '72pt = 1 in'),
+      option({ disabled: true }, ''),
+      option({ value: 'pc' }, 'Pica'),
+      option({ disabled: true }, '6pc = 72pt = 1in'),
+      option({ disabled: true }, ''),
+      option({ value: 'in' }, 'Inches'),
+      option({ disabled: true }, '1in = 96px'),
+      option({ disabled: true }, ''),
+      option({ value: 'cm' }, 'cm'),
+      option({ disabled: true }, '2.54cm = 1in'),
+      option({ disabled: true }, ''),
+      option({ value: 'mm' }, 'mm'),
+      option({ disabled: true }, '25.4mm = 1in'),
+    );
 
     unitSelect.value = this.binder.pageUnit;
-    const unitSwitch = h(".bindery-toggle", "Units", unitSelect);
+    const unitSwitch = row('Units', unitSelect);
 
-    const marginPreview = h(".preview");
-    const marginControl = h(".bindery-val.bindery-margin",
-      h(".top", input.top),
-      h(".inner", input.inner),
-      h(".outer", input.outer),
-      h(".bottom", input.bottom),
+    const marginPreview = h('.preview');
+    const marginControl = h('.bindery-val.bindery-margin',
+      h('.top', input.top),
+      h('.inner', input.inner),
+      h('.outer', input.outer),
+      h('.bottom', input.bottom),
       marginPreview,
     );
 
-    const layoutControl = h(".bindery-layout-control",
+    layoutControl = h('.bindery-layout-control',
       sizeControl,
       marginControl
     );
 
-    const paperSize = h(".bindery-toggle", "Paper Size", h("select",
-      h("option", "Letter"),
-      h("option", { disabled: true }, "8.5 x 11"),
-      h("option", { disabled: true }, ""),
-      h("option", "Legal"),
-      h("option", { disabled: true }, "8.5 x 14"),
-      h("option", { disabled: true }, ""),
-      h("option", "Tabloid"),
-      h("option", { disabled: true }, "11 x 17"),
-      h("option", { disabled: true }, ""),
-      h("option", "A4"),
-      h("option", { disabled: true }, "mm x mm"),
-    ))
+    const paperSize = row('Paper Size', select(
+      option('Letter'),
+      option({ disabled: true }, '8.5 x 11'),
+      option({ disabled: true }, ''),
+      option('Legal'),
+      option({ disabled: true }, '8.5 x 14'),
+      option({ disabled: true }, ''),
+      option('Tabloid'),
+      option({ disabled: true }, '11 x 17'),
+      option({ disabled: true }, ''),
+      option('A4'),
+      option({ disabled: true }, 'mm x mm'),
+    ));
 
-    const perSheet = h(".bindery-toggle", "Pages per Sheet", h("select",
-      h("option", "1"),
-      h("option", "2"),
-    ))
+    const perSheet = row('Pages per Sheet', select(
+      option('1'),
+      option('2'),
+    ));
 
-    const orientation = h(".bindery-toggle", "Orientation", h("select",
-      h("option", "Landscape"),
-      h("option", "Portrait"),
-    ))
+    const orientation = row('Orientation', select(
+      option('Landscape'),
+      option('Portrait'),
+    ));
 
-    const validCheck = h("div", {style: {
-      "display": "none",
-      "color": "#e2b200",
-    }}, "Too Small");
-    const forceRefresh = btnMini({onclick: () => {
-      inProgress.style.display = "block";
-      forceRefresh.style.display = "none";
+    const validCheck = h('div', { style: {
+      display: 'none',
+      color: '#e2b200',
+    } }, 'Too Small');
+    const inProgress = h('div', { style: {
+      display: 'none',
+    } }, 'Updating...');
+    const forceRefresh = btnMini({ onclick: () => {
+      inProgress.style.display = 'block';
+      forceRefresh.style.display = 'none';
       setTimeout(() => {
         this.binder.makeBook(() => {
-          inProgress.style.display = "none";
-          forceRefresh.style.display = "block";
+          inProgress.style.display = 'none';
+          forceRefresh.style.display = 'block';
         }, 100);
       });
-    }}, "Update");
-    const inProgress = h("div", {style: {
-      "display": "none",
-    }}, "Updating...");
-
-    let updateDelay
-    const throttledUpdate = () => {
-      clearTimeout(updateDelay)
-      updateDelay = setTimeout(updateLayout, 700)
-    }
+    } }, 'Update');
 
 
+    let gridMode;
+    let printMode;
+    let interactMode;
     const setGrid = () => {
-      gridMode.classList.add("selected");
-      interactMode.classList.remove("selected");
-      printMode.classList.remove("selected");
+      gridMode.classList.add('selected');
+      interactMode.classList.remove('selected');
+      printMode.classList.remove('selected');
 
-      this.binder.viewer.setGrid()
-    }
+      this.binder.viewer.setGrid();
+    };
     const setInteractive = () => {
-      gridMode.classList.remove("selected");
-      interactMode.classList.add("selected");
-      printMode.classList.remove("selected");
+      gridMode.classList.remove('selected');
+      interactMode.classList.add('selected');
+      printMode.classList.remove('selected');
 
       this.binder.viewer.setInteractive();
-    }
+    };
     const setPrint = () => {
-      gridMode.classList.remove("selected");
-      interactMode.classList.remove("selected");
-      printMode.classList.add("selected");
+      gridMode.classList.remove('selected');
+      interactMode.classList.remove('selected');
+      printMode.classList.add('selected');
 
       this.binder.viewer.setPrint();
-    }
-    const gridMode = h(".bindery-viewmode.grid", { onclick: setGrid }, h(".icon"), "Grid");
-    const interactMode = h(".bindery-viewmode.interactive",  { onclick: setInteractive },  h(".icon"), "Interactive");
-    const printMode = h(".bindery-viewmode.print",  { onclick: setPrint }, h(".icon"), "Sheet");
-    if (this.binder.viewer.mode == "grid") gridMode.classList.add("selected");
-    if (this.binder.viewer.mode == "interactive") interactMode.classList.add("selected");
-    if (this.binder.viewer.mode == "print") printMode.classList.add("selected");
-    const viewSwitcher = h(".bindery-viewswitcher",
+    };
+    gridMode = h('.bindery-viewmode.grid', { onclick: setGrid }, h('.icon'), 'Grid');
+    interactMode = h('.bindery-viewmode.interactive', { onclick: setInteractive }, h('.icon'), 'Interactive');
+    printMode = h('.bindery-viewmode.print', { onclick: setPrint }, h('.icon'), 'Sheet');
+    if (this.binder.viewer.mode === 'grid') gridMode.classList.add('selected');
+    if (this.binder.viewer.mode === 'interactive') interactMode.classList.add('selected');
+    if (this.binder.viewer.mode === 'print') printMode.classList.add('selected');
+    const viewSwitcher = h('.bindery-viewswitcher',
       gridMode, printMode, interactMode,
     );
 
-    const header = h("div", { style : {
-      "padding": "20px",
-      "font-size": "20px"
-    }}, "Bindery");
+    const header = h('div', { style: {
+      padding: '20px',
+      'font-size': '20px',
+    } }, 'Bindery');
 
     const updateLayoutPreview = (newSize, newMargin) => {
       const BASE = 80;
@@ -212,75 +227,75 @@ class Controls {
       ratio = Math.max(ratio, 0.6);
       ratio = Math.min(ratio, 1.8);
 
-      let w, h;
+      let width;
+      let height;
       if (ratio > 2) {
-        w = BASE * ratio;
-        h = BASE;
+        width = BASE * ratio;
+        height = BASE;
+      } else {
+        width = BASE;
+        height = (BASE * 1) / ratio;
       }
-      else {
-        w = BASE;
-        h = BASE * 1 / ratio;
-      }
-      let t = (newMargin.top / newSize.height) * h;
-      let b = (newMargin.bottom / newSize.height) * h;
-      let o = (newMargin.outer / newSize.width) * w;
-      let i =  (newMargin.inner / newSize.width) * w;
+      const t = (newMargin.top / newSize.height) * height;
+      const b = (newMargin.bottom / newSize.height) * height;
+      const o = (newMargin.outer / newSize.width) * width;
+      const i = (newMargin.inner / newSize.width) * width;
 
-      sizeControl.style.width  = `${w}px`;
-      sizeControl.style.height = `${h}px`;
-      marginControl.style.width  = `${w}px`;
-      marginControl.style.height = `${h}px`;
+      sizeControl.style.width = `${width}px`;
+      sizeControl.style.height = `${height}px`;
+      marginControl.style.width = `${width}px`;
+      marginControl.style.height = `${height}px`;
 
-      marginPreview.style.top    = `${t}px`;
+      marginPreview.style.top = `${t}px`;
       marginPreview.style.bottom = `${b}px`;
-      marginPreview.style.left   = `${i}px`;
-      marginPreview.style.right  = `${o}px`;
-    }
+      marginPreview.style.left = `${i}px`;
+      marginPreview.style.right = `${o}px`;
+    };
     updateLayoutPreview(this.binder.pageSize, this.binder.pageMargin);
 
     this.setInProgress = () => {
-      header.innerText = `Paginating...`;
-      validCheck.style.display = "none";
-      inProgress.style.display = "block";
-      forceRefresh.style.display = "none";
-    }
+      header.innerText = 'Paginating...';
+      validCheck.style.display = 'none';
+      inProgress.style.display = 'block';
+      forceRefresh.style.display = 'none';
+    };
 
     this.updateProgress = (count) => {
       header.innerText = `${count} Pages`;
-    }
+    };
 
     this.setDone = () => {
       header.innerText = `${this.binder.viewer.pages.length} Pages`;
-      inProgress.style.display = "none";
-      forceRefresh.style.display = "block";
-      validCheck.style.display = "none";
-    }
+      inProgress.style.display = 'none';
+      forceRefresh.style.display = 'block';
+      validCheck.style.display = 'none';
+    };
 
     this.setInvalid = () => {
-      validCheck.style.display = "block";
-      forceRefresh.style.display = "none";
-      inProgress.style.display = "none";
-    }
+      validCheck.style.display = 'block';
+      forceRefresh.style.display = 'none';
+      inProgress.style.display = 'none';
+    };
 
     const updateLayout = () => {
-      let newMargin = {
-        top:    input.top.value,
-        inner:  input.inner.value,
-        outer:  input.outer.value,
+      const newMargin = {
+        top: input.top.value,
+        inner: input.inner.value,
+        outer: input.outer.value,
         bottom: input.bottom.value,
-      }
-      let newSize = {
+      };
+      const newSize = {
         height: input.height.value,
-        width:  input.width.value
-      }
+        width: input.width.value,
+      };
 
-      let needsUpdate = false
-      for (let k in newMargin) {
-        if (this.binder.pageMargin[k] !== newMargin[k]) { needsUpdate = true }
-      }
-      for (let k in newSize) {
-        if (this.binder.pageSize[k] !== newSize[k]) { needsUpdate = true }
-      }
+      let needsUpdate = false;
+      Object.keys(newMargin).forEach((k) => {
+        if (this.binder.pageMargin[k] !== newMargin[k]) { needsUpdate = true; }
+      });
+      Object.keys(newSize).forEach((k) => {
+        if (this.binder.pageSize[k] !== newSize[k]) { needsUpdate = true; }
+      });
 
       if (needsUpdate) {
         updateLayoutPreview(newSize, newMargin);
@@ -289,41 +304,46 @@ class Controls {
 
         if (this.binder.isSizeValid()) {
           this.binder.makeBook();
-        }
-        else {
+        } else {
           this.setInvalid();
         }
       }
-    }
+    };
 
-    for (let k in input) {
-      input[k].addEventListener("change", throttledUpdate);
-      input[k].addEventListener("keyup", throttledUpdate);
-    }
+    let updateDelay;
+    const throttledUpdate = () => {
+      clearTimeout(updateDelay);
+      updateDelay = setTimeout(updateLayout, 700);
+    };
 
-    const layoutState = h("div",
-      {style: {"float": "right"}},
+    Object.keys(input).forEach((k) => {
+      input[k].addEventListener('change', throttledUpdate);
+      input[k].addEventListener('keyup', throttledUpdate);
+    });
+
+    const layoutState = h('div',
+      { style: { float: 'right' } },
       forceRefresh,
       validCheck,
       inProgress,
-    )
+    );
 
-    this.holder.appendChild(h("div", {},
+    this.holder.appendChild(h('div', {},
         header,
         doneBtn,
         printBtn,
 
-        label(layoutState, "Pagination"),
+        label(layoutState, 'Pagination'),
         layoutControl,
         unitSwitch,
         facingToggle,
 
-        label("Print"),
+        label('Print'),
         paperSize,
         perSheet,
         orientation,
 
-        label("View"),
+        label('View'),
         guidesToggle,
 
         viewSwitcher,
