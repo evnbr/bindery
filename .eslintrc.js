8cb2b511456f30872f3b7c09bb6922ca8ed3b741
Evan Brooks <brooks.ev@gmail.com>
2017-07-21 02:57:11 -0700

    Add ESLint, update dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-console': 0,
+    'func-names': ['error', 'as-needed']
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -6,32 +6,34 @@ module.exports = {
   entry: './src/main.js',
   output: {
     filename: isProd ? 'bindery.min.js' : 'bindery.js',
-    libraryTarget: "var",
-    library: "Bindery",
-    path: __dirname + "/build/",
+    libraryTarget: 'var',
+    library: 'Bindery',
+    path: `${__dirname}/build/`,
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: [
-            ["es2015"]
-          ]
-        }
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['es2015'],
+            ],
+          }
+        },
       },
       {
-          test: /\.svg/,
-          loader: 'svg-url-loader',
-          options: {}
-      }
+        test: /\.svg/,
+        loader: 'svg-url-loader',
+        options: {},
+      },
 
-    ]
-  }
-}
+    ],
+  },
+};
