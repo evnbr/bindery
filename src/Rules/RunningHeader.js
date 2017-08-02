4169013ad8fe0fbeb18d671bf0c93f34a3a89ee9
Evan Brooks <brooks.ev@gmail.com>
2017-08-01 22:35:25 -0700

    Simplify running head rule
    Now bindery gets h1-h6 automatically

--- src/Rules/RunningHeader.js
+++ src/Rules/RunningHeader.js
@@ -4,23 +4,9 @@ import BinderyRule from './BinderyRule';
 class RunningHeader extends BinderyRule {
   constructor(options) {
     options.name = 'Running Header';
-    if (options.beginSection) {
-      options.selector = options.beginSection;
-    }
     super(options);
-    this.customClass = options.customClass;
-    this.currentHeaderContent = '';
-  }
-  afterAdd(elmt, state) {
-    state.currentPage.section = elmt.textContent;
-    return elmt;
   }
   afterBind(page) {
-    if (page.section) {
-      this.currentHeaderContent = page.section;
-    }
-    page.section = this.currentHeaderContent;
-
     const el = h('.bindery-running-header');
     el.innerHTML = this.render(page);
     page.element.appendChild(el);
