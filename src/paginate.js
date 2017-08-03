10f0a78fc40f425732a9a83f94d7170a6cc04374
Evan Brooks <brooks.ev@gmail.com>
2017-08-03 16:10:19 -0700

    Continuations - ignore margin, fix list items

--- src/paginate.js
+++ src/paginate.js
@@ -22,7 +22,12 @@ const clonePath = (origPath) => {
       // restart numbering
       let prevStart = 1;
       if (original.hasAttribute('start')) {
+        // the OL is also a continuation
         prevStart = parseInt(original.getAttribute('start'), 10);
+      }
+      if (i < origPath.length - 1 && origPath[i + 1].tagName === 'LI') {
+        // the first list item is a continuation
+        prevStart -= 1;
       }
       const prevCount = original.children.length;
       const newStart = prevStart + prevCount;
