3601d9277b2396c584f0490bfc69782c4a3fedc6
Evan Brooks <brooks.ev@gmail.com>
2017-08-13 13:06:34 -0700

    Create indexes with PageReference({ createTest })

--- src/Rules/PageReference.js
+++ src/Rules/PageReference.js
@@ -5,6 +5,15 @@ import RuleOption from './RuleOption';
 // selector: String
 // replace: function (HTMLElement, number) => HTMLElement
 
+const makeRanges = arr => arr.reduce((soFar, curr, i) => {
+  if (i === 0) return soFar + curr;
+  const prev = arr[i - 1];
+  if (i === arr.length - 1) return `${soFar}–${curr}`;
+  if (curr === prev + 1) return soFar;
+  if (i === 1) return `${soFar}, ${curr}`;
+  return `${soFar}–${prev}, ${curr}`;
+}, '');
+
 class PageReference extends Replace {
   constructor(options) {
     super(options);
@@ -12,29 +21,24 @@ class PageReference extends Replace {
     this.validate(options, {
       selector: RuleOption.string,
       replace: RuleOption.func,
+      createTest: RuleOption.func,
     });
   }
   afterAdd(elmt, state) {
-    let ref = elmt.getAttribute('href');
-    if (ref) {
-      // TODO: Make more robust, validate
-      // that selector is valid
-      if (ref[0] !== '#') {
-        ref = ref.substr(ref.indexOf('#'));
-      }
-      ref = ref.replace('#', '');
-      ref = `[id="${ref}"]`; // in case it starts with a number
-
+    const test = this.createTest(elmt);
+    if (test) {
       // Temporary, to make sure it'll fit
       const parent = elmt.parentNode;
       const tempClone = elmt.cloneNode(true);
-      const temp = this.replace(tempClone, '⏱');
+      const temp = this.replace(tempClone, '###');
       parent.replaceChild(temp, elmt);
 
-      state.book.firstPageForSelector(ref, (number) => {
+      state.book.onComplete(() => {
         const tempParent = temp.parentNode;
         const finalClone = elmt.cloneNode(true);
-        const newEl = this.replace(finalClone, number);
+        const pageNumbers = state.book.pagesForTest(test);
+        const pageRanges = makeRanges(pageNumbers);
+        const newEl = this.replace(finalClone, pageRanges);
         tempParent.replaceChild(newEl, temp);
       });
 
@@ -42,8 +46,18 @@ class PageReference extends Replace {
     }
     return elmt;
   }
+  createTest(element) {
+    let selector = element.getAttribute('href');
+    if (selector) {
+      selector = selector.replace('#', '');
+      // in case it starts with a number :( thanks wikipedia
+      selector = `[id="${selector}"]`;
+      return el => el.querySelector(selector);
+    }
+    return null;
+  }
   replace(original, number) {
-    original.insertAdjacentHTML('beforeend', ` ↝ Page ${number}`);
+    original.insertAdjacentHTML('beforeend', `, ${number}`);
     return original;
   }
 }
