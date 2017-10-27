510f3e823505d4465c2ef93b5c3ae3be0e7ea975
Evan Brooks <brooks.ev@gmail.com>
2017-10-26 21:56:54 -0700

    Fix version number on error messages

--- src/Viewer/error.js
+++ src/Viewer/error.js
@@ -1,3 +1,5 @@
+/* global BINDERY_VERSION */
+
 import h from 'hyperscript';
 import { c } from '../utils';
 
@@ -5,6 +7,6 @@ export default function (title, text) {
   return h(c('.error'),
     h(c('.error-title'), title),
     h(c('.error-text'), text),
-    h(c('.error-footer'), `Bindery ${'[AIV]{version}[/AIV]'}`),
+    h(c('.error-footer'), `Bindery ${BINDERY_VERSION}`),
   );
 }
