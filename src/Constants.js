223a2747b27c9687853094c37ce9fd21e6b10441
Evan Brooks <brooks.ev@gmail.com>
2018-02-13 13:26:03 -0800

    clean up constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,23 +1,29 @@
-const Mode = {
-  FLIPBOOK: 'bindery-viewmode-flipbook',
-  PREVIEW: 'bindery-viewmode-preview',
-  PRINT: 'bindery-viewmode-print',
-};
+const Mode = Object.freeze({
+  FLIPBOOK: 'view_flipbook',
+  PREVIEW: 'view_preview',
+  PRINT: 'view_print',
+});
 
-const Paper = {
-  AUTO: 'size_page',
-  AUTO_BLEED: 'size_page_bleed',
-  AUTO_MARKS: 'size_page_marks',
-  LETTER_PORTRAIT: 'size_letter_p',
-  LETTER_LANDSCAPE: 'size_letter_l',
-  A4_PORTRAIT: 'size_a4_p',
-  A4_LANDSCAPE: 'size_a4_l',
-};
+const Paper = Object.freeze({
+  AUTO: 'paper_auto',
+  AUTO_BLEED: 'paper_auto_bleed',
+  AUTO_MARKS: 'paper_auto_marks',
+  LETTER_PORTRAIT: 'paper_letter_p',
+  LETTER_LANDSCAPE: 'paper_letter_l',
+  A4_PORTRAIT: 'paper_a4_p',
+  A4_LANDSCAPE: 'paper_a4_l',
+});
 
-const Layout = {
-  PAGES: 'pages',
-  SPREADS: 'spreads',
-  BOOKLET: 'booklet',
-};
+const Layout = Object.freeze({
+  PAGES: 'layout_pages',
+  SPREADS: 'layout_spreads',
+  BOOKLET: 'layout_booklet',
+});
 
-export { Mode, Paper, Layout };
+const Marks = Object.freeze({
+  NONE: 'marks_none',
+  SPREADS: 'layout_spreads',
+  BOTH: 'marks_both',
+});
+
+export { Mode, Paper, Layout, Marks };
