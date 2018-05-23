326362adf3aafeab33031eae51590e9de3f9e4f1
Evan Brooks <brooks.ev@gmail.com>
2018-05-22 18:34:59 -0700

    relax new eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,12 @@ module.exports = {
     'prefer-arrow-callback': ['error', { 'allowNamedFunctions': true }],
     'no-unused-vars': ['warn'],
     'no-console': 0,
+    'object-curly-newline': 0,
     'class-methods-use-this': 0,
     'func-names': ['error', 'as-needed'],
     'no-param-reassign': ['error', { 'props': false }],
     'no-await-in-loop': 'off',
+    'no-restricted-syntax': 0,
+    'prefer-destructuring': 0,
   },
 };
