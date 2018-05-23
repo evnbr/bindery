17cb7a64ae79d26527233df137b83a5ee2909fe3
Evan Brooks <brooks.ev@gmail.com>
2018-05-22 22:07:03 -0700

    css-length tests

--- src/css-length/index.js
+++ src/css-length/index.js
@@ -3,6 +3,8 @@ const cssNumberRegEx = /^([+-]?[0-9]+(.?[0-9]+)?)(px|in|cm|mm|pt|pc)$/;
 const isLength = str => cssNumberRegEx.test(str);
 
 const parseLength = (str) => {
+  if (!isLength(str)) throw Error(`Cannot parse css length from "${str}"`);
+
   const matches = str.match(cssNumberRegEx);
   return {
     val: Number(matches[1]),
