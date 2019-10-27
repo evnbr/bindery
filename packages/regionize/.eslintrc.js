1d69ecd676471984911f5817348be7ca6bc601c6
Evan Brooks <brooks.ev@gmail.com>
2019-10-27 09:53:58 -0700

    restore regionize pkg config

---
+++ packages/regionize/.eslintrc.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+  "env": {
+    "browser": true,
+    "node": true,
+    "jest": true
+  },
+  extends: 'airbnb-base',
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prefer-arrow-callback': ['error', { 'allowNamedFunctions': true }],
+    'no-unused-vars': ['warn'],
+    'no-console': 0,
+    'object-curly-newline': 0,
+    'class-methods-use-this': 0,
+    'func-names': ['error', 'as-needed'],
+    'no-param-reassign': ['error', { 'props': false }],
+    'no-await-in-loop': 'off',
+    'no-restricted-syntax': 0,
+    'prefer-destructuring': 0,
+  },
+};
