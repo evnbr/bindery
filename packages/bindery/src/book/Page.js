7274ab939d824a331685db9d3981614f883e887d
Evan Brooks <brooks.ev@gmail.com>
2019-11-03 17:10:01 -0800

    renderpage in own file

--- packages/bindery/src/book/Page.js
+++ packages/bindery/src/book/Page.js
@@ -1,52 +1,5 @@
 import { Region } from 'regionize';
-import { safeMeasure, createEl, classes, c } from '../dom-utils';
-
-// TODO: renderPage needs to be called
-// after any change to pageInfo that affects
-// the size of the flow region. These changes
-// need to be flushed before regionize tries
-// to measure the element again.
-const renderPage = (pageInfo) => {
-  const {
-    flowRegion,
-    isLeft,
-    isSpread,
-    isSpacer,
-    suppress,
-    rotation,
-    backgroundContent,
-    footnoteElements,
-    runningHeader,
-  } = pageInfo;
-
-  let backgroundEl = createEl('page-background');
-  const footerEl = createEl('footer');
-  if (backgroundContent) {
-    backgroundEl.append(backgroundContent);
-    if (rotation && rotation !== 'none') {
-      const rotateContainer = createEl(`.rotate-container.page-size-rotated.rotate-${rotation}`);
-      rotateContainer.append(backgroundEl);
-      backgroundEl = rotateContainer;
-    }
-  }
-  const pg = createEl('page', [
-    backgroundEl,
-    flowRegion.element,
-    footerEl,
-  ]);
-  if (footnoteElements) {
-    footerEl.append(...footnoteElements);
-  }
-  if (runningHeader) {
-    pg.append(runningHeader);
-  }
-  pg.classList.toggle(classes.leftPage, isLeft);
-  pg.classList.toggle(classes.rightPage, !isLeft);
-  if (suppress) pg.classList.add(classes.isOverflowing);
-  if (isSpread) pg.classList.add(c('spread'));
-  if (isSpacer) pg.style.visibility = 'hidden';
-  return pg;
-};
+import { safeMeasure, createEl } from '../dom-utils';
 
 class Page {
   constructor() {
